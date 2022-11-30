fd21f575f635951bd23efbb0b99d2cc9741414e0
inderjit5 <114800326+inderjit5@users.noreply.github.com>
2022-11-30 21:05:45 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,23 @@
+from functools import update_wrapper
+from tkinter.font import BOLD
+from turtle import*
+bgcolor('white')
+color('red')
+begin_fill()
+pensize(5)
+setposition(0,0)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+penup()
+setposition(0,200)
+pendown()
+write("love from INDERJIT",font=("Verdana",15, "bold"), align="center")
+hideturtle()
+done() 
+
+
